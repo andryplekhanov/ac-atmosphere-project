3f1e6328c832d767b869e3f4f46a7587efcc2a01
Andrey Plekhanov <1989plekhanov@gmail.com>
2023-08-07 19:17:03 +0300

    admin's answer added

--- tgbot/keyboards/inline.py
+++ tgbot/keyboards/inline.py
@@ -1,5 +1,6 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+from tgbot.misc.factories import for_answer
 
 # Клавиатура с выбором действия
 personal_data_choice = InlineKeyboardMarkup(
@@ -12,3 +13,21 @@ personal_data_choice = InlineKeyboardMarkup(
         ],
     ]
 )
+
+
+def answer_from_admin(user_id: str) -> InlineKeyboardMarkup:
+    """
+    Клавиатура с кнопками для администратора - выбор действия с сообщением.
+    """
+
+    keyboard = InlineKeyboardMarkup(row_width=1)
+
+    keyboard.add(InlineKeyboardButton(
+        text='Ответить',
+        callback_data=for_answer.new(user_id=user_id, action='answer')
+    ))
+    keyboard.add(InlineKeyboardButton(
+        text='Спам (блокировать)',
+        callback_data=for_answer.new(user_id=user_id, action='spam')
+    ))
+    return keyboard
---
+++ tgbot/misc/factories.py
@@ -0,0 +1,3 @@
+from aiogram.utils.callback_data import CallbackData
+
+for_answer = CallbackData("answer", "user_id", "action")
